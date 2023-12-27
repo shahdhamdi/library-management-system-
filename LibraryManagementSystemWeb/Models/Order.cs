705c70123b987f701c64195858e49606599bc52e
shahd hamdi <shahd.hamdi@gmail.com>
2023-12-28 01:09:24 +0200

    Third commit

--- LibraryManagementSystemWeb/Models/Order.cs
+++ LibraryManagementSystemWeb/Models/Order.cs
@@ -1,9 +1,14 @@
-﻿namespace LibraryManagementSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagementSystem.Models
 {
     public class Order
     {
         public int Id { get; set; }
-        public string PhoneNumber {  get; set; }    
+        [Required]
+        public string PhoneNumber {  get; set; }
+
+        [Required]
         public string Address { get; set; }
         public double totalPrice {  get; set; }
 
